aed54e81def1b8b54ca1febe8dc4d9300a6613b9
Roman Simecek <roman@good2go.ch>
2019-10-24 14:34:57 +0200

    Setup gem.

---
+++ lib/sequel/oracle_enhanced.rb
@@ -0,0 +1,8 @@
+require "sequel/oracle_enhanced/version"
+
+module Sequel
+  module OracleEnhanced
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sequel/oracle_enhanced/version.rb
@@ -0,0 +1,5 @@
+module Sequel
+  module OracleEnhanced
+    VERSION = "0.1.0"
+  end
+end
